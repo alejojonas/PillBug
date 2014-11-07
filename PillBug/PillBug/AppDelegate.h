d0feada3eaffdfc42617037073dd54793a3b0d5a
Jonas <alejo.jonas@gmail.com>
2014-11-07 15:13:37 -0800

    Initialize project

---
+++ PillBug/PillBug/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PillBug
+//
+//  Created by Kay Lab on 11/7/14.
+//  Copyright (c) 2014 Team10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PillBug/PillBug/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PillBug
+//
+//  Created by Kay Lab on 11/7/14.
+//  Copyright (c) 2014 Team10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
