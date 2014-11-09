be9e7f9346c845ca04807691a1b750cb0d671369
Jonas <alejo.jonas@gmail.com>
2014-11-09 00:27:59 -0800

    Patient UI

---
+++ PillBug/PillBug/PrescriptionInfo.h
@@ -0,0 +1,14 @@
+//
+//  PrescriptionInfo.h
+//  PillBug
+//
+//  Created by Kay Lab on 11/9/14.
+//  Copyright (c) 2014 Team10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PrescriptionInfo : UIViewController
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *backBtn;
+
+@end
---
+++ PillBug/PillBug/PrescriptionInfo.m
@@ -0,0 +1,52 @@
+//
+//  PrescriptionInfo.m
+//  PillBug
+//
+//  Created by Kay Lab on 11/9/14.
+//  Copyright (c) 2014 Team10. All rights reserved.
+//
+
+#import "PrescriptionInfo.h"
+
+@interface PrescriptionInfo ()
+
+@end
+
+@implementation PrescriptionInfo
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)BackBtn:(id)sender {
+    [self dismissModalViewControllerAnimated:YES];
+}
+@end
