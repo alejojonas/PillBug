33b0d035439806884a8e48d75339e7b4564ae844
Jonas <alejo.jonas@gmail.com>
2014-11-09 00:05:39 -0800

    AddPatient

---
+++ PillBug/PillBug/AddPatient.h
@@ -0,0 +1,19 @@
+//
+//  AddPatient.h
+//  PillBug
+//
+//  Created by Kay Lab on 11/8/14.
+//  Copyright (c) 2014 Team10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/PFLogInViewController.h>
+
+@interface AddPatient : UIViewController <UITabBarDelegate, UITableViewDataSource, PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate>{
+    IBOutlet UITableView *tableView;
+    
+    NSArray * mainArray;
+}
+- (IBAction)backBtn:(id)sender;
+
+@end
