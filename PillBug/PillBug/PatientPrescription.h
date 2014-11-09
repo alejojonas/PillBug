d8a7ffbfbe68ecdcfecd25b563ba2b4883ecdfcf
Jonas <alejo.jonas@gmail.com>
2014-11-09 00:13:59 -0800

    PatientPrescription

---
+++ PillBug/PillBug/PatientPrescription.h
@@ -0,0 +1,19 @@
+//
+//  PatientPrescription.h
+//  PillBug
+//
+//  Created by Kay Lab on 11/8/14.
+//  Copyright (c) 2014 Team10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/PFLogInViewController.h>
+
+@interface PatientPrescription : UIViewController <UITabBarDelegate, UITableViewDataSource, PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate>{
+    IBOutlet UITableView *tableView;
+    
+    NSArray * mainArray;
+}
+- (IBAction)backBtn:(id)sender;
+
+@end
