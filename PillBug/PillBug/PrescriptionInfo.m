95229def65f81c0a981ed7b70c1372d7bcd6b30a
Jonas <alejo.jonas@gmail.com>
2014-11-17 12:38:52 -0800

    improve patient day aesthetics

--- PillBug/PillBug/PrescriptionInfo.m
+++ PillBug/PillBug/PrescriptionInfo.m
@@ -49,6 +49,22 @@
     friday.enabled = NO;
     saturday.enabled = NO;
     
+    sunday.layer.borderWidth = .5f;
+    sunday.layer.cornerRadius = 5;
+    monday.layer.borderWidth = .5f;
+    monday.layer.cornerRadius = 5;
+    tuesday.layer.borderWidth = .5f;
+    tuesday.layer.cornerRadius = 5;
+    wednesday.layer.borderWidth = .5f;
+    wednesday.layer.cornerRadius = 5;
+    thursday.layer.borderWidth = .5f;
+    thursday.layer.cornerRadius = 5;
+    friday.layer.borderWidth = .5f;
+    friday.layer.cornerRadius = 5;
+    saturday.layer.borderWidth = .5f;
+    saturday.layer.cornerRadius = 5;
+
+    
     PFQuery *query = [PFQuery queryWithClassName:@"Drugs"];
     [query whereKey:@"drugName" equalTo:drugName];
     [query getFirstObjectInBackgroundWithBlock:^(PFObject *object, NSError *error) {
@@ -71,12 +87,46 @@
             dayArray = [object objectForKey:@"days"];
             timeArray = [object objectForKey:@"times"];
             [sunday setSelected:[[dayArray objectAtIndex:0]boolValue]];
+            if(sunday.selected == true){
+                [sunday.layer setBackgroundColor:[UIColor colorWithRed:0.0 green:122.0/255.0 blue:1.0 alpha:1.0].CGColor];
+                [sunday.titleLabel setTextColor:[UIColor whiteColor]];
+            }
             [monday setSelected:[[dayArray objectAtIndex:1]boolValue]];
+            if(monday.selected == true){
+                [monday.layer setBackgroundColor:[UIColor colorWithRed:0.0 green:122.0/255.0 blue:1.0 alpha:1.0].CGColor];
+                [monday.titleLabel setTextColor:[UIColor whiteColor]];
+
+            }
             [tuesday setSelected:[[dayArray objectAtIndex:2]boolValue]];
+            if(tuesday.selected == true){
+                [tuesday.layer setBackgroundColor:[UIColor colorWithRed:0.0 green:122.0/255.0 blue:1.0 alpha:1.0].CGColor];
+                [tuesday.titleLabel setTextColor:[UIColor whiteColor]];
+
+            }
             [wednesday setSelected:[[dayArray objectAtIndex:3]boolValue]];
+            if(wednesday.selected == true){
+                [wednesday.layer setBackgroundColor:[UIColor colorWithRed:0.0 green:122.0/255.0 blue:1.0 alpha:1.0].CGColor];
+                [wednesday.titleLabel setTextColor:[UIColor whiteColor]];
+
+            }
             [thursday setSelected:[[dayArray objectAtIndex:4]boolValue]];
+            if(thursday.selected == true){
+                [thursday.layer setBackgroundColor:[UIColor colorWithRed:0.0 green:122.0/255.0 blue:1.0 alpha:1.0].CGColor];
+                [thursday.titleLabel setTextColor:[UIColor whiteColor]];
+
+            }
             [friday setSelected:[[dayArray objectAtIndex:5]boolValue]];
+            if(friday.selected == true){
+                [friday.layer setBackgroundColor:[UIColor colorWithRed:0.0 green:122.0/255.0 blue:1.0 alpha:1.0].CGColor];
+                [friday.titleLabel setTextColor:[UIColor whiteColor]];
+
+            }
             [saturday setSelected:[[dayArray objectAtIndex:6]boolValue]];
+            if(saturday.selected == true){
+                [saturday.layer setBackgroundColor:[UIColor colorWithRed:0.0 green:122.0/255.0 blue:1.0 alpha:1.0].CGColor];
+                [saturday.titleLabel setTextColor:[UIColor whiteColor]];
+
+            }
             
             NSMutableString *results = [NSMutableString stringWithString:@""];
             
